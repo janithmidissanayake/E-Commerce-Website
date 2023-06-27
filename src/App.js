3ad6cf51c22d5db945c24af8205d380e26a849e7
janithmidissanayake <dissanayakejw.20@uom.lk>
2023-06-27 06:11:30 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+       <a href="/">Amazona</a>
+      </header>
+      <main>List odf Products</main>
+    </div>
+  );
+}
+
+export default App;
